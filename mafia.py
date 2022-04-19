9172035905d100b4a6b9231c5d846a4ad1942e50
группа2 <anvarg04@gmail.com>
2022-04-19 17:35:42 +0500

    123

---
+++ mafia.py
@@ -0,0 +1,4 @@
+def mafia(context):
+    job = context.job
+    context.bot.send_message(job.context, text='1234567899')
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import ReplyKeyboardMarkup
 from telegram import ReplyKeyboardRemove
+from mafia import mafia
 
 # Запускаем логгирование
 logging.basicConfig(
@@ -20,14 +21,14 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 # Напишем соответствующие функции.
 def start(update, context):
-    print(123455)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Ты - то, что ты ешь")
     update.message.reply_text(
         f"Я бот-справочник. Какая информация вам нужна?",
         reply_markup=markup
     )
 
 
-def help(update, context):
+def help1(update, context):
     update.message.reply_text(
         "Я - бот-справочник.")
 
@@ -58,6 +59,43 @@ def close_keyboard(update, context):
     )
 
 
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    started_mafia = True
+    chat_id = update.message.chat_id
+    try:
+        due = 45
+        if due < 0:
+            update.message.reply_text('Извините, не умеем возвращаться в прошлое')
+            return
+
+        # Добавляем задачу в очередь
+        # и останавливаем предыдущую (если она была)
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(mafia, due, context=chat_id, name=str(chat_id))
+
+        text = f'Регистрация будет идти 45 секунд'
+        if job_removed:
+            text += ' Старая задача удалена.'
+        update.message.reply_text(text)
+        registrated = True
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Использование: /set <секунд>')
+
+
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
@@ -65,9 +103,14 @@ def main():
     dp.add_handler(CommandHandler("phone", phone))
     dp.add_handler(CommandHandler("site", site))
     dp.add_handler(CommandHandler("work_time", work_time))
-    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("help", help1))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("close", close_keyboard))
+    dp.add_handler(CommandHandler("start_mafia", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+
     updater.start_polling()
 
     updater.idle()
